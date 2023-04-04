c9d09c98edf0d1c8caf2bc7f92e2a175045a002c
Ivan <developerjsua@gmail.com>
2023-04-04 18:43:42 +0300

    replaced from XMLHTTPRequest to fetch

--- js/script.js
+++ js/script.js
@@ -278,30 +278,32 @@ window.addEventListener('DOMContentLoaded', () => {
 			`
 			form.append(statusMessage)
 
-			const request = new XMLHttpRequest()
 			const formData = new FormData(form)
-
-			request.open('POST', 'server.php')
-			request.setRequestHeader('Content-type', 'application/json')
 
 			const object = {}
 			formData.forEach(function (value, key) {
 				object[key] = value
 			})
-			const json = JSON.stringify(object)
 
-			request.send(json)
-
-			request.addEventListener('load', () => {
-				if (request.status === 200) {
-					console.log(request.response)
-					showThanksModal(message.success)
-					form.reset()
-					statusMessage.remove()
-				} else {
-					showThanksModal(message.failure)
-				}
+			fetch('server.php', {
+				method: 'POST',
+				headers: {
+					'Content-type': 'application/json',
+				},
+				body: JSON.stringify(object),
 			})
+				.then(data => data.text())
+				.then(data => {
+					console.log(data)
+					showThanksModal(message.success)
+					statusMessage.remove()
+				})
+				.catch(() => {
+					showThanksModal(message.failure)
+				})
+				.finally(() => {
+					form.reset()
+				})
 		})
 	}
 
