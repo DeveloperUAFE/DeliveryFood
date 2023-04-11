d375a4c38bfc2449b037cec336dbd1864f2830ef
Ivan <developerjsua@gmail.com>
2023-04-11 21:18:58 +0300

    customized calculator

--- js/script.js
+++ js/script.js
@@ -412,13 +412,44 @@ window.addEventListener('DOMContentLoaded', () => {
 	}
 
 	// calculator
-
+	console.log(+localStorage.getItem('ratio'))
 	const result = document.querySelector('.calculating__result span')
-	let sex = 'female',
-		height,
-		weight,
-		age,
+	let sex, height, weight, age, ratio
+
+	if (localStorage.getItem('sex')) {
+		sex = localStorage.getItem(sex)
+	} else {
+		sex = 'female'
+		localStorage.setItem('sex', 'female')
+	}
+
+	if (localStorage.getItem('ratio')) {
+		ratio = localStorage.getItem('ratio')
+	} else {
 		ratio = 1.375
+		localStorage.setItem('ratio', 1.375)
+	}
+
+	function initLocalSettings(selector, activeClass) {
+		const elements = document.querySelectorAll(selector)
+
+		elements.forEach(elem => {
+			elem.classList.remove(activeClass)
+			if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+				elem.classList.add(activeClass)
+			} else if (
+				elem.getAttribute('data-ratio') === localStorage.getItem('ratio')
+			) {
+				elem.classList.add(activeClass)
+			}
+		})
+	}
+
+	initLocalSettings('#gender div', 'calculating__choose-item_active')
+	initLocalSettings(
+		'.calculating__choose_big div',
+		'calculating__choose-item_active'
+	)
 
 	function calcTotal() {
 		if (!sex || !height || !weight || !age || !ratio) {
@@ -439,15 +470,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	calcTotal()
 
-	function getStaticInformation(parentSelector, activeClass) {
-		const elements = document.querySelectorAll(`${parentSelector} div`)
+	function getStaticInformation(selector, activeClass) {
+		const elements = document.querySelectorAll(selector)
 
 		elements.forEach(item => {
 			item.addEventListener('click', element => {
 				if (element.target.getAttribute('data-ratio')) {
 					ratio = +element.target.getAttribute('data-ratio')
+					localStorage.setItem('ratio', ratio)
 				} else {
 					sex = element.target.id
+					localStorage.setItem('sex', sex)
 				}
 				elements.forEach(item => {
 					item.classList.remove(activeClass)
@@ -459,9 +492,9 @@ window.addEventListener('DOMContentLoaded', () => {
 		})
 	}
 
-	getStaticInformation('#gender', 'calculating__choose-item_active')
+	getStaticInformation('#gender div', 'calculating__choose-item_active')
 	getStaticInformation(
-		'.calculating__choose_big',
+		'.calculating__choose_big div',
 		'calculating__choose-item_active'
 	)
 
@@ -469,6 +502,12 @@ window.addEventListener('DOMContentLoaded', () => {
 		const input = document.querySelector(inputSelector)
 
 		input.addEventListener('input', () => {
+			if (input.value.match(/\D/gi)) {
+				input.style.border = '1px solid red'
+			} else {
+				input.style.border = ''
+			}
+
 			switch (input.getAttribute('id')) {
 				case 'height':
 					height = +input.value
